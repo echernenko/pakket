6b46b4df07ed7f16fc1c4a511cd8f96e5d934106
Eugene Chernenko <eugene.chernenko@booking.com>
2018-04-18 14:58:56 +0200

    Adding /status endpoint to pakket to display list of modules in UI
     - template is having mock data at the moment
     - TODO list is documented

--- lib/Pakket/Web/App.pm
+++ lib/Pakket/Web/App.pm
@@ -28,6 +28,32 @@ sub setup {
     foreach my $repo_config ( @{ $config->{'repositories'} } ) {
         Pakket::Web::Repo->create($repo_config);
     }
+
+    get '/info' => sub {
+        my @repositories =  map { { 'type' => $_->{'type'},
+                                    'path' => $_->{'path'} } }
+                                @{ $config->{'repositories'} };
+        return encode_json({
+                'version' => $Pakket::Web::App::VERSION,
+                'repositories' => [@repositories],
+                });
+    };
+
+    # status page
+    get '/status' => sub {
+        # the data should be processed here
+        # like on ine 28
+        # foreach my $repo_config
+        # and we need TODO:
+        # 1. form array of modules (by merging spec and sources)
+        # 2. cut "perl/" prefix
+        # 3. loop over parcel repos and get data regarding parcels
+        # 4. create a hash to be consumed by UI for rendering a table
+        my $dirname = dirname(__FILE__);
+        my $template = Path::Tiny::path($dirname.'/views/status.html');
+        return $template->slurp_utf8;
+    };
+
 }
 
 1;
